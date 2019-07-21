738b75e5a3979cc6db853355f1034503fe7f09d6
MAHESHPRIYA <maheshpriyamca@gmail.com>
2019-07-21 13:08:37 +0530

    committed

--- src/test/java/com/app/tests/HomePageTest.java
+++ src/test/java/com/app/tests/HomePageTest.java
@@ -75,7 +75,7 @@ public class HomePageTest {
 		homePage.setTextemail(email);
 		String msg = homePage.sucess();
 
-		Assert.assertTrue(msg.contains("You have successfully subscribed to this newsletter"), "successfully");
+		Assert.assertTrue(msg.contains("You have successfully subscribed to this newsletter"), "success fully");
 
 	}
 
